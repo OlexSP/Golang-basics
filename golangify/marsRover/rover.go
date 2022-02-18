d042ea8f043f987263626367c501333b79e5694d
Alex Al <alexsputnik@gmail.com>
2022-02-18 12:42:24 +0200

    rover.go

--- golangify/marsRover/rover.go
+++ golangify/marsRover/rover.go
@@ -6,17 +6,25 @@ import (
 	"time"
 )
 
-type command int
+func main() {
+	r := NewRoverDriver()
+	time.Sleep(3 * time.Second)
+	r.Left()
+	time.Sleep(3 * time.Second)
+	r.Right()
+	time.Sleep(3 * time.Second)
+	r.Stop()
+	time.Sleep(3 * time.Second)
+	r.Start()
+	time.Sleep(3 * time.Second)
+}
 
-const (
-	right = command(0)
-	left  = command(1)
-)
-
+// RoverDriver ведет марсоход по поверхности Марса
 type RoverDriver struct {
 	commandc chan command
 }
 
+// NewRoverDriver начинает новый RoverDriver и возвращает его.
 func NewRoverDriver() *RoverDriver {
 	r := &RoverDriver{
 		commandc: make(chan command),
@@ -25,14 +33,26 @@ func NewRoverDriver() *RoverDriver {
 	return r
 }
 
+type command int
+
+const (
+	right = command(0)
+	left  = command(1)
+	start = command(2)
+	stop  = command(3)
+)
+
+// drive отвечает за вождение марсохода. Ожидается
+// что он будет начинаться в горутине.
 func (r *RoverDriver) drive() {
-	pos := image.Point{X: 0, Y: 0}       // current position
-	direction := image.Point{X: 1, Y: 0} // current direction
+	pos := image.Point{X: 0, Y: 0}
+	direction := image.Point{X: 1, Y: 0}
 	updateInterval := 250 * time.Millisecond
-	nextMove := time.After(updateInterval) // make initial timer channel
+	nextMove := time.After(updateInterval)
+	speed := 1
 	for {
 		select {
-		case c := <-r.commandc: // waits for the new comands
+		case c := <-r.commandc:
 			switch c {
 			case right:
 				direction = image.Point{
@@ -44,40 +64,36 @@ func (r *RoverDriver) drive() {
 					X: direction.Y,
 					Y: -direction.X,
 				}
+			case stop:
+				speed = 0
+			case start:
+				speed = 1
 			}
-			log.Printf("new direction %v", direction)
-		case <-nextMove: // wait for the timer
-			pos = pos.Add(direction)
-			log.Printf("moved to %v", pos)        // outputs position
-			nextMove = time.After(updateInterval) // makes the next timer channel
+			log.Printf("new direction %v; speed %d", direction, speed)
+		case <-nextMove:
+			pos = pos.Add(direction.Mul(speed))
+			log.Printf("moved to %v", pos)
+			nextMove = time.After(updateInterval)
 		}
 	}
 }
 
-// turns the rover left
+// Left поворачивает марсоход налево (90° потив часовой стрелки).
 func (r *RoverDriver) Left() {
 	r.commandc <- left
 }
 
-// turns the rover right
+// Right поворачивает марсоход направо (90° по часовой стрелке).
 func (r *RoverDriver) Right() {
 	r.commandc <- right
 }
 
-func main() {
-	r := NewRoverDriver()
-	time.Sleep(2 * time.Second)
-	r.Left()
-	time.Sleep(2 * time.Second)
-	r.Left()
-	time.Sleep(2 * time.Second)
-	r.Left()
-	time.Sleep(2 * time.Second)
-	r.Right()
-	time.Sleep(2 * time.Second)
-	r.Right()
-	time.Sleep(2 * time.Second)
-	r.Right()
-	time.Sleep(2 * time.Second)
+// Stop останавливает марсоход.
+func (r *RoverDriver) Stop() {
+	r.commandc <- stop
+}
 
+// Start начинает движение марсохода.
+func (r *RoverDriver) Start() {
+	r.commandc <- start
 }
